9f1bc3818b41b10699966bfd9acaf3e5da10ecf6
unknown <nayara-eng@hotmail.com>
2022-08-23 14:29:40 -0300

    implementação da função delete e estilicação com css

--- base/main.js
+++ base/main.js
@@ -1,27 +1,20 @@
 let employees = [];
 let roles = [];
 let selectedItem;
-const listEl = document.querySelector("ul")
-const formEl = document.querySelector("form")
-const bdelete = document.getElementById("bdelete")
-const bcancel = document.getElementById("bcancel")
-const bsubmit = document.getElementById("bsubmit")
-
+const listEl = document.querySelector("ul");
+const formEl = document.querySelector("form");
+const bdelete = document.getElementById("bdelete");
+const bcancel = document.getElementById("bcancel");
+const bsubmit = document.getElementById("bsubmit");
 
 async function init() {
-  try {
-    [employees, roles] = await Promise.all([
-      listEmployees(),
-      listRoles()]);
-    renderRoles()
-    renderData();
-    clearSelection();
-    bcancel.addEventListener("click",clearSelection)
-    
-    
-  } catch (erro) {
-    showError(erro);
-  }
+  [employees, roles] = await Promise.all([listEmployees(), listRoles()]);
+  renderRoles();
+  renderData();
+  clearSelection();
+  bcancel.addEventListener("click", clearSelection);
+  formEl.addEventListener("submit", onSubmit);
+  bdelete.addEventListener("click", onDelete);
 }
 init();
 
@@ -31,17 +24,16 @@ function selectItem(employee, li) {
   li.classList.add("selected");
   formEl.name.value = employee.name;
   formEl.salary.valueAsNumber = employee.salary;
-  formEl.role_id.value = employee.rode_id;
+  formEl.role_id.value = employee.role_id;
   bdelete.style.display = "inline";
   bcancel.style.display = "inline";
-
-
+  bsubmit.textContent = "Update";
 }
 
 function clearSelection() {
+  clearError();
   selectedItem = undefined;
-  const li = listEl.querySelector(".selected")
-
+  const li = listEl.querySelector(".selected");
   if (li) {
     li.classList.remove("selected");
   }
@@ -49,12 +41,49 @@ function clearSelection() {
   formEl.salary.value = "";
   formEl.role_id.value = "";
   bdelete.style.display = "none";
-  bcancel.style.display = "none"
+  bcancel.style.display = "none";
+  bsubmit.textContent = "Create";
+}
+
+async function onSubmit(evt) {
+  evt.preventDefault();
+  const employeeData = {
+    name: formEl.name.value,
+    salary: formEl.salary.valueAsNumber,
+    role_id: +formEl.role_id.value,
+  };
+  if (!employeeData.name || !employeeData.salary || !employeeData.role_id) {
+    showError("You must fill all form fields.");
+  } else {
+    if (selectedItem) {
+      const updatedItem = await updateEmployee(selectedItem.id, employeeData);
+      const i = employees.indexOf(selectedItem);
+      employees[i] = updatedItem;
+      renderData();
+      selectItem(updatedItem, listEl.children[i]);
+    } else {
+      const createdItem = await createEmployee(employeeData);
+      employees.push(createdItem);
+      renderData();
+      selectItem(createdItem, listEl.lastChild);
+      listEl.lastChild.scrollIntoView();
+    }
+  }
+}
+
+async function onDelete() {
+  if (selectedItem) {
+    await deleteEmployee(selectedItem.id);
+    const i = employees.indexOf(selectedItem);
+    employees.splice(i, 1);
+    renderData();
+    clearSelection();
+  }
 }
 
 function renderData() {
-
-  for(const employee of employees) {
+  listEl.innerHTML = "";
+  for (const employee of employees) {
     let role = roles.find((role) => role.id == employee.role_id);
     const li = document.createElement("li");
     const divName = document.createElement("div");
@@ -69,8 +98,7 @@ function renderData() {
 }
 
 function renderRoles() {
-
-  for(const role of roles) {
+  for (const role of roles) {
     const option = document.createElement("option");
     option.textContent = role.name;
     option.value = role.id;
@@ -78,28 +106,13 @@ function renderRoles() {
   }
 }
 
-function showError(error) {
-  document.getElementById("errors").textContent = "Erro ao carregar dados.";
-  console.error(error);
+function showError(message, error) {
+  document.getElementById("errors").textContent = message;
+  if (error) {
+    console.error(error);
+  }
 }
 
-/*
-// Criar
-fetch(`http://localhost:3000/employees`, {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify(employee),
-});
-
-// Atualizar
-fetch(`http://localhost:3000/employees/${id}`, {
-  method: "PUT",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify(employee),
-});
-
-// Excluir
-fetch(`http://localhost:3000/employees/${id}`, {
-  method: "DELETE",
-});
-*/
+function clearError() {
+  document.getElementById("errors").textContent = "";
+}
